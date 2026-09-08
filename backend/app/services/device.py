d846c1314b59c913830a1ed45ccda0bdaf9a2437
mwahajkhalil <mwahajkhalil@gmail.com>
2026-09-08 21:35:01 +0200

    impr

--- backend/app/services/device.py
+++ backend/app/services/device.py
@@ -1,4 +1,4 @@
-from app.schemas.device import DeviceResponse
+from app.schemas.device import DeviceResponse, DeviceCreate
 from typing import Optional
 
 class DeviceService: #this class will be used to perform business logic on devices
@@ -15,12 +15,29 @@ class DeviceService: #this class will be used to perform business logic on devic
         return self._devices
 
     
-    def find_device(self, device_id:int) -> Optional[DeviceResponse]:
+    def get_device(self, device_id:int) -> Optional[DeviceResponse]:
         for d in self._devices:
             if d.id == device_id:
                 return d
         return None 
+    
+    
+    def create_device(self, device:DeviceCreate)-> DeviceResponse:
+        new_id = max([d.id for d in self._devices] + [0]) + 1
+      #  new_device = DeviceResponse(**device.model_dump(), id=new_id)
+      
+        new_device = DeviceResponse(
+            id=new_id,
+            name=device.name,
+            status=device.status,
+        )
+
+        self._devices.append(new_device)
+        return new_device
+    
+    
         
+
     
 
 
